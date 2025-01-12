df65a5fae26da897da835712af178b644aa1537b
Leo Cheng <leocheng@Leos-MacBook-Pro-2.local>
2025-01-12 09:48:12 -0500

    completed project

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const digitButtons = document.querySelectorAll(".digit");
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
 
+
+const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
+
+
+
 const setInputText = (str) => {
     userInput.value = str;
 }
@@ -11,8 +16,32 @@ const getInputText = () => {
     return userInput.value;
 }
 
-const checkValid = (str) => {
-    
+const checkValid = (msg) => regex.test(msg);
+
+const validate = () => {
+    const inpt = userInput.value;
+    if(inpt === "") {
+        alert("Please provide a phone number");
+        return;
+    }
+    resultDiv.innerHTML += checkValid(inpt) ? `
+    <p class="log-res">Valid US number: ${inpt}</p>
+    <hr class="hr-result">` :`
+    <p class="log-res">Invalid US number: ${inpt}</p>
+    <hr class="hr-result">`;
+
+    userInput.value = "";
+}
+
+const clearResults = () => {
+    resultDiv.innerHTML = "";
 }
 
 digitButtons.forEach((button) => button.addEventListener("click", () => {setInputText(getInputText() + button.textContent)}));
+checkButton.addEventListener("click", validate);
+clearButton.addEventListener("click", clearResults);
+userInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        validate();
+    }
+})
